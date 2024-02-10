71104fd40c8c2d0d30102dcfb34d01e8c6dfcc09
fanasadas <144501508+fanasadas@users.noreply.github.com>
2024-02-09 23:55:46 -0800

    Photo viewing feature

--- script.js
+++ script.js
@@ -4,3 +4,55 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function initializeTyped(selector, delay, strings) {
+    return new Typed(selector, {
+        startDelay: delay,
+        strings: strings,
+        typeSpeed: 100,
+        loop: false,
+        preStringTyped: function(arrayPos, self) {
+            showCursor(selector);
+        },
+        onComplete: function(self) {
+            hideCursor(selector);
+        }
+    });
+}
+
+function showCursor(selector) {
+    const cursor = document.querySelector(selector + ' + .typed-cursor');
+    if (cursor) {
+        cursor.style.opacity = 1;
+    }
+}
+
+function hideCursor(selector) {
+    const cursor = document.querySelector(selector + ' + .typed-cursor');
+    if (cursor) {
+        cursor.style.display = 'none';
+    }
+}
+
+initializeTyped(".typing-name", 1500, ["Eric Fan"]);
+initializeTyped(".typing-photographer", 3000, ["Photographer"]);
+initializeTyped(".typing-swe", 4700, ["Software Engineer"]);
+
+function expandPhoto(src) {
+    const overlay = document.getElementById("photo-overlay");
+    const expandedPhoto = document.getElementById("expanded-photo");
+    expandedPhoto.src = src;
+    overlay.style.display = "block";
+}
+
+function closeExpandedPhoto(event) {
+    if (event.target.id === "photo-overlay") {
+        document.getElementById("photo-overlay").style.display = "none";
+    }
+}
+
+document.querySelectorAll(".photo").forEach(photo => {
+    photo.addEventListener("click", function() {
+        expandPhoto(this.src);
+    });
+});
